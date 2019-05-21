0491507da7a1b266c30960f0c3e6daa29c3e5170
zac <zac_hsing@mail.chungyo.net>
2019-05-21 10:06:15 +0800

    seisiki

--- test/main/loginToLobby.js
+++ test/main/loginToLobby.js
@@ -2,8 +2,8 @@
 module.exports = {
     "testing": browser => {
         browser
-        .url('http://google.com')
-        .waitForElementPresent('#fbar > div > div > div > span')
+        .url('https://ahab.inlottodev.com/')
+        .waitForElementPresent('#userName')
         // .waitForElementPresent('.a',1000,'not found')
         .end()
     }
