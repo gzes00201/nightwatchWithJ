f797d54b46dfd33160bccdf8b45b3ff1ac61531d
PIN大 <gzes00201@gmail.com>
2019-06-03 10:59:22 +0800

    Update nightwatch.conf.js

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -17,7 +17,7 @@ const config = {
     "start_process": false,
     "server_path": seleniumServer.path,
     "log_path": "",
-    "host": "127.0.0.1",
+    "host": "172.19.0.8",
     "port": 4444,
     "cli_args": {
       "webdriver.chrome.driver": "./bin/chromedriver",
@@ -36,7 +36,7 @@ const config = {
         "selenium" : {
           "start_process" : true
         },
-        "selenium_host": "127.0.0.1",
+        "selenium_host": "172.19.0.8",
         "silent": true,
         "screenshots": {
           "enabled": false,
